261fa4705b1d485bff2d1c026886a3bd3051f8ed
Tiffany Xiao <cdtiffanyxiao@gmail.com>
2021-03-20 22:12:04 -0400

    Changed fixed projects on page to dynamic
    Changed the fixed projects on the page to be created automatically using Javascript to make creating more projects easier.

--- modal_script.js
+++ modal_script.js
@@ -1,23 +1,86 @@
-for (let i=1; i < 5; i++){
-    // Get the modal
-    let modal = document.getElementById("myModal"+i);
+data.forEach((project) => {
+    let newModal = document.createElement("div");
+    newModal.className = "modal-style";
 
-    // Get the button that opens the modal
-    let btn = document.getElementById("myBtn"+i);
+    let newModalCoverImage = document.createElement("img");
+    newModalCoverImage.id = "myBtn"+project["btnID"];
+    newModalCoverImage.src = project["coverImage"];
+    newModalCoverImage.className = "image-with-border";
+    newModal.appendChild(newModalCoverImage);
 
-    // Get the <span> element that closes the modal
-    let span = document.getElementsByClassName("close")[i-1];
+    let newModalBreak = document.createElement("br");
+    newModal.appendChild(newModalBreak);
+
+    let newModalTitle = document.createElement("p");
+    newModalTitle.innerHTML = project["name"];
+    newModal.appendChild(newModalTitle);
+
+    let newModalContentBox = document.createElement("div");
+    newModalContentBox.id = "myModal"+project["modalId"];
+    newModalContentBox.className = "modal";
+
+    let newModalContent= document.createElement("div");
+    newModalContent.className = "modal-content";
+
+    let newModalSpan= document.createElement("span");
+    newModalSpan.className = "close";
+    newModalSpan.innerHTML = "&times";
+    newModalContent.appendChild(newModalSpan);
+
+    let newModalInnerImage= document.createElement("img");
+    newModalInnerImage.className = "modal-inner-image";
+    newModalInnerImage.src = project["innerImage"];
+    newModalContent.appendChild(newModalInnerImage);
+
+    let newModalh1= document.createElement("h1");
+    newModalh1.innerHTML = project["name"];
+    newModalContent.appendChild(newModalh1);
+
+    let newModalDateCompleted= document.createElement("h2");
+    newModalDateCompleted.innerHTML = "Date Completed: "+project["dateCompleted"];
+    newModalContent.appendChild(newModalDateCompleted);
+
+    let newModalPartners= document.createElement("h2");
+    newModalPartners.innerHTML = "Partner(s): "+project["partners"];
+    newModalContent.appendChild(newModalPartners);
+
+    let newModalSkills= document.createElement("h2");
+    newModalSkills.innerHTML = "Skills Used:"+project["skillsUsed"];
+    newModalContent.appendChild(newModalSkills);
+
+    let newModalPara= document.createElement("p");
+    newModalPara.className = "modal-p-style";
+    newModalContent.appendChild(newModalPara);
+
+    project["paragraphs"].forEach((paragraph) =>{
+        let tempPara= document.createElement("p");
+        tempPara.innerHTML = paragraph;
+        newModalContent.appendChild(tempPara);
+        // newModalContent.appendChild(newModalBreak);
+        // newModalContent.appendChild(newModalBreak);
+    });
+
+    let newModalLink= document.createElement("a");
+    newModalLink.href = project["link"];
+    newModalLink.innerHTML = project["link"];
+    newModalContent.appendChild(newModalLink);
+
+    newModalContentBox.appendChild(newModalContent);
+    newModal.appendChild(newModalContentBox);
+
+    let divOfModals = document.getElementById("projects-rows");
+    divOfModals.appendChild(newModal);
 
     // When the user clicks the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
+    newModalCoverImage.onclick = function() {
+        newModalContentBox.style.display = "block";
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
+    newModalSpan.onclick = function() {
+        newModalContentBox.style.display = "none";
     }
-}
+});
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
---
+++ projects.js
@@ -0,0 +1,62 @@
+let data = [
+    {
+        "name": "Modeling Child Congition",
+        "btnID": "1",
+        "coverImage": "Project/modeling_child_cognition.jpg",
+        "innerImage": "Project/modeling_child_cognition2.png",
+        "modalId": "1",
+        "dateCompleted": "April 22, 2019",
+        "partners": "Professor Peter De Villiers",
+        "skillsUsed": "R, Multiple Rgression",
+        "paragraphs": ["For this project, I worked with Professor Peter De Villiers to build models for exploring Theory of Mind differences between monolingual and bilingual children.", "Children grow in a variety of environments with different factors that can affect their development. From socioeconomic factors to bilingualism, there is interest on how these factors can affect a child’s development. In particular, there has been much research focused on the development of children’s Theory of Mind (ToM), or how a child is able to understand their own and others’ minds. In this project, we explored the potential impacts of factors such as language and executive functioning performance in children’s development. Specifically, we sought to analyze the question: What is the role of executive functioning and language skills in theory of mind performance in bilingual versus monolingual children?", "Contact me for the research paper."],
+        "link": ""
+    },
+    {
+        "name": "GenNow: A Simple Website Generator",
+        "btnID": "2",
+        "coverImage": "Project/gennow.jpg",
+        "innerImage": "Project/gennow2.jpg",
+        "modalId": "2",
+        "dateCompleted": "May 8, 2018",
+        "partners": "Khadidja Fares",
+        "skillsUsed": "Javascript, HTML/CSS, Design",
+        "paragraphs": ["GenNow was built with the purpose of helping non-profit organizations be able to quickly and easily generate static websites to display their organization's information.", "Contact me if you'd like to see our final paper on this project.", "GENerate your website NOW at"],
+        "link": "https://tiffanyxiao.com/nonprofit-website-generator/index.html"
+    },
+    {
+        "name": "Predicting Airbnb Superhost Status",
+        "btnID": "3",
+        "coverImage": "Project/wordcloud.png",
+        "innerImage": "Project/airbnb_poster.png",
+        "modalId": "3",
+        "dateCompleted": "December 15, 2017",
+        "partners": "Shirley Xu",
+        "skillsUsed": "R, Machine Learning, Design",
+        "paragraphs": ["The objective of this project is to attempt to find the factors that can best predict whether a host is a superhost on Airbnb.", "We utilized two modeling techniques - logistic regression and classification tree. The logistic regression model correctly predicted superhost status 70.66% of the time on the test set, with a sensitivity value of 0.6748 and a specificity value of 0.7395. The classification tree correctly predicted superhost status 83.87% of the time on the test set, with the 3 predictor variables used in the tree construction are `number_of_reviews`, `host_total_listings_count`, and `review_scores_rating`.", "Both modeling techniques identified `number_of_reviews` and `review_scores_rating` as the most significant predictors in predicting whether or not a host is a superhost. Thus, the most reliable way to predict superhost status is most likely by finding out how many 5-star reviews a host has.", "Link to download the academic poster we presented"],
+        "link": "Project/airbnb_poster.pdf"
+    },
+    {
+        "name": "True Stories of Dr. Seuss",
+        "btnID": "4",
+        "coverImage": "Project/drseuss.jpg",
+        "innerImage": "Project/lorax_and_tiff.JPG",
+        "modalId": "4",
+        "dateCompleted": "December 12, 2016",
+        "partners": "None",
+        "skillsUsed": "Adobe Premiere + Photoshop + Illustrator",
+        "paragraphs": ["As part of one of my academic final projects, I was told to write a paper on anything I discovered in the town of my college (Northampton, MA).", "For this topic, I chose to write about a Dr. Seuss exhibit I found in the town's local museum. Dr. Seuss's hometown is Springfield, MA, which is less than 10 minutes away from Northampton.", "Contact me for the full research paper", "As part of the project, I also created a youtube video summarizing my findings. You can find the youtube video at"],
+        "link": "https://www.youtube.com/watch?v=9fD3JglPQXs"
+    },
+    // {
+    //     "name": "Name",
+    //     "btnID": "1",
+    //     "coverImage": "image.jpg",
+    //     "innerImage": "image2.jpg",
+    //     "modalId": "1",
+    //     "dateCompleted": "Month Date, Year",
+    //     "partners": "PartnerName",
+    //     "skillsUsed": "Javascript",
+    //     "paragraphs": ["para1", "para2", "para3"],
+    //     "link": ""
+    // },
+];
